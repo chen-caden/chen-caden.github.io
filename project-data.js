5e1d5f24733ff8d57df4e37c6d0c389d2cd906cd
caden c <cadenc.woss@gmail.com>
2024-08-03 11:19:55 -0400

    Update project-data.js

--- project-data.js
+++ project-data.js
@@ -4,6 +4,11 @@ const projects = [
         description: "A LSTM model built and trained on python using the tensorflow library.",
         link: "https://github.com/sakufish/temperature-model"
     },
+    {
+        name: "Automatic TicTacToe bot",
+        description: "Uses OpenCV to detect game board and pieces",
+        link: "https://github.com/sakufish/TicTacToe-recognition-bot"
+    },
     { 
         name: "Pytris", 
         description: "A replica of Tetris on python using the pygame library.",

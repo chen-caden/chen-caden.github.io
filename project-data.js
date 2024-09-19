43ea6a0797a19e46fbc87450ecf24f91dcf66aca
caden chen <cadenc.woss@gmail.com>
2024-09-18 21:26:10 -0400

    Update project-data.js

--- project-data.js
+++ project-data.js
@@ -5,6 +5,11 @@ const projects = [
         link: "https://github.com/sakufish/stock-model"
     },
     { 
+        name: "InstaLearn", 
+        description: "A react web app designed to replace short-form media with an educational content feed. AI-powered learning path tree diagrams and quiz content generator. Built with Node.js, MongoDB, Gemini API.",
+        link: "https://github.com/chrisma71/InstaLearn"
+    },
+    { 
         name: "myFridge - myFridgeApp.vercel.app", 
         description: "A React web app for creating and managing personalized nutritional plans. Built with Node.js, MongoDB Atlas, and integrated with the Gemini API. Includes smart photo and upload features.",
         link: "https://github.com/chrisma71/myFridge"
@@ -18,10 +23,5 @@ const projects = [
         name: "Pytris", 
         description: "A replica of Tetris on python using the pygame library.",
         link: "https://github.com/sakufish/pytris" 
-    },
-    { 
-        name: "Timeseries Temperature Model", 
-        description: "A LSTM model built and trained on python using the tensorflow library.",
-        link: "https://github.com/sakufish/temperature-model"
     },
 ];

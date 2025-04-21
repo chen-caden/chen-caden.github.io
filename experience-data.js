540a70757af16f4181180b8059f10f0eea3b2b0f
caden chen <cadenc.woss@gmail.com>
2025-04-20 20:55:08 -0400

    Update experience-data.js

--- experience-data.js
+++ experience-data.js
@@ -3,19 +3,19 @@ const experiences = [
         time: "June 2024 - Present",
         title: "Vice President of Operations",
         place: "White Oaks DECA",
-        description: "Led a 30+ member executive team in managing operations for a 500+ member DECA chapter. Coordinated internal and external student registration and logistics with Ontario DECA. Managed and monitored the records of 500+ students across several platforms, implementing automated flighting using scripts."
+        description: "Managed operations for a 500+ member DECA chapter, leading 30+ executives. Coordinated internal and external student registration and logistics with Ontario DECA. Managed and monitored the records of 500+ students, implementing automated flighting using scripts."
     },    
     {
         time: "September 2024 - Present",
         title: "Hockey Referee",
         place: "Hockey Canada, OMHA",
-        description: "Officiated formal Hockey games across 6 Rep and House League divisions."
+        description: "Officiated formal Hockey games across 6 Rep and House League divisions. I get yelled at :)"
     },    
     {
         time: "December 2024 - Present",
         title: "Ignition Hacks Organizer",
         place: "Ignition Hacks",
-        description: "Coordinated logistics for one of the largest Canadian hackathons for university and high school students."
+        description: "Coordinating logistics for one of the largest Canadian hackathons for university and high school students."
     },
     {
         time: "June 2024 - Present",

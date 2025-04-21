301d0dc6cf51aaef93fa65a3b652f07e9e5ca49a
caden chen <cadenc.woss@gmail.com>
2025-04-20 21:01:58 -0400

    Update experience-data.js

--- experience-data.js
+++ experience-data.js
@@ -9,31 +9,18 @@ const experiences = [
         time: "September 2024 - Present",
         title: "Hockey Referee",
         place: "Hockey Canada, OMHA",
-        description: "Officiated formal Hockey games across 6 Rep and House League divisions. I get yelled at :)"
-    },    
-    {
-        time: "December 2024 - Present",
-        title: "Ignition Hacks Organizer",
-        place: "Ignition Hacks",
-        description: "Coordinating logistics for one of the largest Canadian hackathons for university and high school students."
-    },
-    {
-        time: "June 2024 - Present",
-        title: "Vice President of Logistics",
-        place: "White Oaks FBLA",
-        description: "Managed internal logistics for FBLA, overseeing registrations and performance data. Implemented scripts to automate and streamline processes."
+        description: "Officiating formal Hockey games across 6 Rep and House League divisions. I get yelled at a lot :)"
     },
     {
         time: "June 2024 - Present",
         title: "Vice President of Logistics",
         place: "White Oaks Debate Team",
-        description: "Managed the logistics of 130+ club members, implementing JavaScript into Google Sheets to automate debate round slating. Judged and provided feedback on debate rounds."
+        description: "Managed the logistics of 130+ club members, implementing JavaScript into Sheets to automate debate round slating. Cut processing time by over 90%. Judged and provided feedback on debate rounds."
     },
-
     {
-        time: "July 2023 - Present",
-        title: "Skate Instructor",
-        place: "Oak Medical Education Foundation",
-        description: "Taught skating to children ages 4-12, and tailored teaching style to fit each age group. Provided feedback and encouragement."
+        time: "December 2024 - Present",
+        title: "Ignition Hacks Organizer",
+        place: "Ignition Hacks",
+        description: "Coordinating logistics for one of the largest Canadian hackathons for university and high school students."
     },
 ];

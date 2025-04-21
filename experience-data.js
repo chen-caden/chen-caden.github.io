9bf47c29cd45b6f6c7ec312f9d823a69043f2040
caden chen <cadenc.woss@gmail.com>
2025-04-20 20:51:39 -0400

    Update experience-data.js

--- experience-data.js
+++ experience-data.js
@@ -3,31 +3,31 @@ const experiences = [
         time: "June 2024 - Present",
         title: "Vice President of Operations",
         place: "White Oaks DECA",
-        description: "Led a 30+ member executive team in managing operations for a 500+ member DECA chapter. Coordinated internal and external student registration and logistics with DECA Ontario and our school advisor. Managed and monitored the records of 500+ students across several platforms, implementing automated flighting using scripts."
+        description: "Led a 30+ member executive team in managing operations for a 500+ member DECA chapter. Coordinated internal and external student registration and logistics with Ontario DECA. Managed and monitored the records of 500+ students across several platforms, implementing automated flighting using scripts."
     },    
     {
         time: "September 2024 - Present",
         title: "Hockey Referee",
         place: "Hockey Canada, OMHA",
-        description: "Officiated formal Hockey games across several divisions."
+        description: "Officiated formal Hockey games across 6 Rep and House League divisions."
     },    
     {
         time: "December 2024 - Present",
         title: "Ignition Hacks Organizer",
         place: "Ignition Hacks",
-        description: "Part of the logistics team for one of the largest Canadian hackathons for university and high school students."
+        description: "Coordinated logistics for one of the largest Canadian hackathons for university and high school students."
     },
     {
         time: "June 2024 - Present",
         title: "Vice President of Logistics",
         place: "White Oaks FBLA",
-        description: "Managed internal logistics for FBLA, overseeing registrations, outreach, and orders. Implemented scripts to automate and streamline processes to improve efficiency."
+        description: "Managed internal logistics for FBLA, overseeing registrations and performance data. Implemented scripts to automate and streamline processes."
     },
     {
         time: "June 2024 - Present",
         title: "Vice President of Logistics",
         place: "White Oaks Debate Team",
-        description: "Managed the logistics of 130+ club members, implementing automated solutions to debate round slating. Judged and provided feedback on debate rounds. Google Sheets and JavaScript."
+        description: "Managed the logistics of 130+ club members, implementing JavaScript into Google Sheets to automate debate round slating. Judged and provided feedback on debate rounds."
     },
 
     {
@@ -35,11 +35,5 @@ const experiences = [
         title: "Skate Instructor",
         place: "Oak Medical Education Foundation",
         description: "Taught skating to children ages 4-12, and tailored teaching style to fit each age group. Provided feedback and encouragement."
-    },
-    {
-        time: "Unknown - March 2024",
-        title: "Competitive Swimmer",
-        place: "Oakville Aquatic Club",
-        description: "Swam a lot"
     },
 ];
